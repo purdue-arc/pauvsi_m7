7ea18a8b88f47061163aa7b24e2ef6f21c6d95d3
Trexter <sheridak@purdue.edu>
2016-05-05 18:33:45 -0400

    Fixed the CMakeLists to find the executable

--- src/Utilities/PGCameraDriver.cpp
+++ src/Utilities/PGCameraDriver.cpp
@@ -4,9 +4,6 @@
 
 #define DEFAULT_RATE 10
 
-FlyCapture2::Camera cam;
-FlyCapture2::CameraInfo cInfo;
-
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "CameraDriver", ros::init_options::AnonymousName); // initializes with a randomish name
@@ -15,6 +12,43 @@ int main(int argc, char **argv)
 	int rate;
 	ros::param::param<int>("frame_rate", rate, DEFAULT_RATE);
 	ros::Rate loop_rate(rate);
+
+	//setup camera
+	FlyCapture2::Error error;
+	FlyCapture2::Camera camera;
+	FlyCapture2::CameraInfo camInfo;
+
+	// Connect the camera
+	{
+		error = camera.Connect( 0 );
+		if ( error != FlyCapture2::PGRERROR_OK )
+		{
+			ROS_INFO("Failed to connect to camera\n");
+			return false;
+		}
+
+		// Get the camera info and print it out
+		error = camera.GetCameraInfo( &camInfo );
+		if ( error != FlyCapture2::PGRERROR_OK )
+		{
+			ROS_INFO("Failed to get camera info from camera\n");
+			return false;
+		}
+
+		std::cout << "Model: " << camInfo.modelName << std::endl;
+
+		error = camera.StartCapture();
+		if ( error == FlyCapture2::PGRERROR_ISOCH_BANDWIDTH_EXCEEDED )
+		{
+			ROS_INFO("Bandwidth exceeded\n");
+			return false;
+		}
+		else if ( error != FlyCapture2::PGRERROR_OK )
+		{
+			ROS_INFO("Failed to start image capture\n");
+			return false;
+		}
+	}
 
 	//the main loop
 	while(nh.ok())
