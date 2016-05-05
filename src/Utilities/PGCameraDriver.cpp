c2e6cfec5810d2d4aaa78edc4cc516ea36bef044
Trexter <sheridak@purdue.edu>
2016-05-05 00:21:15 -0400

    Now use params to set frame rate

--- src/Utilities/PGCameraDriver.cpp
+++ src/Utilities/PGCameraDriver.cpp
@@ -1,12 +1,28 @@
-#include <ros.h>
+#include <ros/ros.h>
+#include <ros/console.h>
 #include <flycapture/FlyCapture2.h>
+
+#define DEFAULT_RATE 10
 
 FlyCapture2::Camera cam;
 FlyCapture2::CameraInfo cInfo;
 
-int main()
+int main(int argc, char **argv)
 {
-	cam.GetCameraInfo(&cInfo);
+	ros::init(argc, argv, "CameraDriver", ros::init_options::AnonymousName); // initializes with a randomish name
+	ros::NodeHandle nh; // create the node handler
+
+	int rate;
+	ros::param::param<int>("frame_rate", rate, DEFAULT_RATE);
+	ros::Rate loop_rate(rate);
+
+	//the main loop
+	while(nh.ok())
+	{
+
+		//SLEEP
+		loop_rate.sleep();
+	}
 
 	return 0;
 }
