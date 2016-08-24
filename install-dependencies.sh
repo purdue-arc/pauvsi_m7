2d43311afc0972070fca72152a19bd7d7cd73da1
Trexter <sheridak@purdue.edu>
2016-08-23 22:29:51 -0400

    updated the install bash script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,30 @@
-cd ~/catkin_ws/src
+#Dependencies for ethzasl_ptam
+sudo apt-get install freeglut3-dev liblapacke-dev liblapack3 libopenblas-base libopenblas-dev liblapack-dev
 
-git clone https://github.com/ros-perception/image_pipeline
+# INSTALL TooN
+cd ~/
+git clone https://github.com/edrosten/TooN
+cd TooN
+./configure && make && sudo make install
+cd ..
+
+
+# Installing gvars
+git clone https://github.com/edrosten/gvars
+cd gvars
+./configure && make && sudo make install
+
+
+# Installing libcvd
+cd ~/
+git clone https://github.com/edrosten/libcvd
+cd libcvd
+./configure && make && sudo make install
+make test
+
+#installing ethzasl_ptam
+cd ~/catkin_ws/src
+git clone https://github.com/ethz-asl/ethzasl_ptam
+cd ..
+
+catkin_make
